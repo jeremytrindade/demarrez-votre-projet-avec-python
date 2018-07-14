5ad14efa300be7c05a324a7d9a872cbe3ecbbc26
Jérémy DA TRINDADE <jeremytrindade@gmail.com>
2018-07-14 16:11:21 +0200

    Creation du dossier avec des functions interessantes a propos des array

---
+++ store_lists.py
@@ -0,0 +1,12 @@
+# liste avec les donner de base
+characters = ["alvin et les Chipmunks", "Babar", "betty boop", "calimero", "casper", "le chat potté", "Kirikou"]
+print("La liste avec tout les donner : ", characters)
+
+# pour savoir l'emplacement d'un element:
+print("Pour savoir l'emplacement d'un element: characters.index('Babar')")
+characters.index("Babar")
+
+# pour ajouter un element a la fin de la liste:
+print("Pour ajouter un element a la fin de la liste: characters.append('Jérémy')")
+characters.append("Jérémy")
+print("Maintenant c'est comme ça : ", characters)
