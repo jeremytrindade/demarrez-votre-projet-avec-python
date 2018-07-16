27a2fb69a5b9c7f5e3c326fbe754a5e3236d7d98
Jérémy DA TRINDADE <jeremytrindade@gmail.com>
2018-07-16 13:18:47 +0200

    Le fichier avec les base en Python

---
+++ rand_json.py
@@ -0,0 +1,43 @@
+import random
+
+quotes = [
+    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
+    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+]
+
+characters = [
+    "alvin et les Chipmunks", 
+    "Babar", 
+    "betty boop", 
+    "calimero", 
+    "casper", 
+    "le chat potté", 
+    "Kirikou"
+]
+# Read values from a JSON file
+def read_value_from_json():
+  # Create a new empty list
+  # Open a json file with my objects
+  # Load all the data contained in my file
+  # add each item in my list
+  # return my completed list
+
+def get_random_quote(my_list):
+  rand_numb = random.randint(0,len(my_list)-1)
+  item = my_list[rand_numb]
+  return item
+    
+def capitalize(words):
+  for word in words:
+    word.capitalize()
+
+def message(character, quote):
+  capitalize(character)
+  capitalize(quote)
+  return "{} a dit : {}".format(character, quote)
+
+user_answer = input("Tapez entrée pour conaitre une autre citation ou B pour quitter le programe.")
+
+while user_answer != "B":
+  print(message(get_random_quote(characters), get_random_quote(quotes)))
+  user_answer = input("Tapez entrée pour conaitre une autre citation ou B pour quitter le programe.")
